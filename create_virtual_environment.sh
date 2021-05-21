8c95342d63da792029aacffce62627b0917a8efb
Orla Mallon <52678852+Orlz@users.noreply.github.com>
2021-05-21 09:52:59 +0200

    Update create_virtual_environment.sh

--- create_virtual_environment.sh
+++ create_virtual_environment.sh
@@ -6,11 +6,15 @@ python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
+# problems when installing from requirements.txt
 pip install ipython
 pip install jupyter
+pip install matplotlib
+pip install opencv-python
+
 python -m ipykernel install --user --name=$VENVNAME
 
-test -f requirements.txt && pip install -r requirements.txt
+test -f requirements.txt && pip install requirements.txt
 
 deactivate
 echo "build $VENVNAME"
